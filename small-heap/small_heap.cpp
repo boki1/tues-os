e6e87f246a2f297317727d816a6d211e7e09bd3d
Kristiyan Stoimenov <kristoimenov@gmail.com>
2023-10-29 02:07:01 +0300

    small-heap+format: Add clang-format spec and format small-heap/
    Signed-off-by: Kristiyan Stoimenov <kristoimenov@gmail.com>

--- small-heap/small_heap.cpp
+++ small-heap/small_heap.cpp
@@ -10,16 +10,19 @@
  * Helper functions
  */
 
-std::size_t small_heap::ceil_p2(std::uintptr_t value, std::size_t p2) {
+std::size_t small_heap::ceil_p2(std::uintptr_t value, std::size_t p2)
+{
     return value + p2 - 1 & -p2;
 }
 
-std::size_t small_heap::floor_p2(std::uintptr_t value, std::size_t p2) {
+std::size_t small_heap::floor_p2(std::uintptr_t value, std::size_t p2)
+{
     return value & -p2;
 }
 
-bool small_heap::is_last_block(void *bl_header) {
-    return *reinterpret_cast<std::uint64_t *>(bl_header) != 0;
+bool small_heap::is_last_block(void* bl_header)
+{
+    return *reinterpret_cast<std::uint64_t*>(bl_header) != 0;
 }
 
 /*
@@ -28,7 +31,8 @@ bool small_heap::is_last_block(void *bl_header) {
  * header itself and there should be no allocations taking place there.
  */
 
-std::optional<small_heap::chunk> small_heap::small_allocate(size_t count_pages, size_t count_chunks_in_last) {
+std::optional<small_heap::chunk> small_heap::small_allocate(size_t count_pages, size_t count_chunks_in_last)
+{
     assert(count_chunks_in_last < CHUNKS_IN_BLOCK);
     // The number of pages in a block coincidentally is
     // equal to the number of chunks in a block.
@@ -44,7 +48,7 @@ std::optional<small_heap::chunk> small_heap::small_allocate(size_t count_pages, 
         // The start is at 1 because the first uintptr_t is unused, since it corresponds to the
         // exact page which contains the bitmap.
         for (size_t found_count = 0, i = 1; i < CHUNKS_IN_BLOCK; ++i) {
-            const std::uintptr_t bigword = *reinterpret_cast<std::uintptr_t *>(block_addr + i);
+            const std::uintptr_t bigword = *reinterpret_cast<std::uintptr_t*>(block_addr + i);
             if (found_count == count_pages) {
                 if (!bigword || __builtin_clzll(bigword) > count_chunks_in_last)
                     return found_first_idx;
@@ -52,7 +56,7 @@ std::optional<small_heap::chunk> small_heap::small_allocate(size_t count_pages, 
             }
             if (!bigword) {
                 if (found_first_idx < 0)
-                    found_first_idx = i;    // Safety: i < CHUNKS_IN_BLOCK so no loss could happen due to int type conversion.
+                    found_first_idx = i;// Safety: i < CHUNKS_IN_BLOCK so no loss could happen due to int type conversion.
                 ++found_count;
                 if (found_count == count_pages && count_chunks_in_last == 0)
                     return found_first_idx;
@@ -64,7 +68,7 @@ std::optional<small_heap::chunk> small_heap::small_allocate(size_t count_pages, 
     };
 
     auto mark_chunks_used = [count_pages, count_chunks_in_last](uintptr_t block_addr, size_t idx) {
-        auto *bigword = reinterpret_cast<std::uintptr_t *>(block_addr + idx);
+        auto* bigword = reinterpret_cast<std::uintptr_t*>(block_addr + idx);
         for (size_t i = 0; i < count_pages; ++i, ++bigword) {
             assert(!*bigword);
             *bigword = ~0;
@@ -77,25 +81,26 @@ std::optional<small_heap::chunk> small_heap::small_allocate(size_t count_pages, 
     };
 
     for (std::uintptr_t block_addr = m_reserved; block_addr < m_reserved_end; block_addr += BLOCK_SIZE) {
-        TRY_OR (!mprotect(reinterpret_cast<void *>(block_addr), PAGE_SIZE, PROT_WRITE), "mprotect PROT_WRITE");
+        TRY_OR(!mprotect(reinterpret_cast<void*>(block_addr), PAGE_SIZE, PROT_WRITE), "mprotect PROT_WRITE");
         if (const ssize_t idx_in_block = fit_in_block(block_addr); idx_in_block > 0) {
             mark_chunks_used(block_addr, idx_in_block);
             return chunk{.addr = block_addr + (idx_in_block << CHUNK_BITS),
-                    .size = (count_chunks_in_last << CHUNK_BITS) + (count_pages << PAGE_BITS)};
+                         .size = (count_chunks_in_last << CHUNK_BITS) + (count_pages << PAGE_BITS)};
         }
     }
 
     return {};
 }
 
-std::optional<small_heap::chunk> small_heap::tiny_allocate(size_t count_chunks) {
+std::optional<small_heap::chunk> small_heap::tiny_allocate(size_t count_chunks)
+{
     assert(count_chunks < CHUNKS_IN_PAGE);
 
     auto fit_in_block_and_mark_used = [count_chunks](std::uintptr_t block_addr) -> ssize_t {
         const std::uintptr_t lsb_mask = ((1 << count_chunks) - 1);
         const std::uintptr_t mask = lsb_mask << (CHUNKS_IN_PAGE - count_chunks);
         for (size_t i = 1; i < CHUNKS_IN_BLOCK; ++i) {
-            auto *bigword_ptr = reinterpret_cast<std::uintptr_t *>(block_addr + i);
+            auto* bigword_ptr = reinterpret_cast<std::uintptr_t*>(block_addr + i);
             const std::uintptr_t bigword = *bigword_ptr;
             for (size_t j = 0; j < CHUNKS_IN_PAGE; ++j) {
                 const std::uintptr_t bigword_shifted = bigword << j;
@@ -114,21 +119,22 @@ std::optional<small_heap::chunk> small_heap::tiny_allocate(size_t count_chunks) 
 
     for (std::uintptr_t block_addr = m_reserved; block_addr < m_reserved_end; block_addr += BLOCK_SIZE) {
         // Make the header of the block currently inspected usable.
-        TRY_OR (!mprotect(reinterpret_cast<void *>(block_addr), PAGE_SIZE, PROT_WRITE), "mprotect PROT_WRITE");
+        TRY_OR(!mprotect(reinterpret_cast<void*>(block_addr), PAGE_SIZE, PROT_WRITE), "mprotect PROT_WRITE");
         if (const ssize_t idx_in_block = fit_in_block_and_mark_used(block_addr); idx_in_block > 0) {
             const uintptr_t chunk_addr = block_addr + (idx_in_block << CHUNK_BITS);
 
             // Make the allocated space usable - PAGE_SIZE is used as in tiny_allocate the sizes of
             // the allocations are strictly less than the size of the page.
             const auto chunk_addr_aligned = floor_p2(chunk_addr, PAGE_SIZE);
-            TRY_OR (!mprotect(reinterpret_cast<void *>(chunk_addr_aligned), PAGE_SIZE, PROT_WRITE), "mprotect PROT_WRITE");
+            TRY_OR(!mprotect(reinterpret_cast<void*>(chunk_addr_aligned), PAGE_SIZE, PROT_WRITE), "mprotect PROT_WRITE");
             return chunk{.addr = chunk_addr, .size = count_chunks << CHUNK_BITS};
         }
     }
     return {};
 }
 
-std::optional<small_heap::chunk> small_heap::allocate(std::size_t desired_size) {
+std::optional<small_heap::chunk> small_heap::allocate(std::size_t desired_size)
+{
     const auto size = ceil_p2(desired_size, CHUNK_SIZE);
     if (desired_size > MAX_ALLOCATEBLE_SIZE)
         return {};
@@ -142,8 +148,8 @@ std::optional<small_heap::chunk> small_heap::allocate(std::size_t desired_size) 
 
     return count_full_pages == 0
             ? tiny_allocate(count_in_last_page)
-            : small_allocate(count_full_pages,count_in_last_page);
+            : small_allocate(count_full_pages, count_in_last_page);
 }
 
 // TODO:
-void free(small_heap::chunk &&allocated_chunk) {}
+void free(small_heap::chunk&& allocated_chunk) {}
