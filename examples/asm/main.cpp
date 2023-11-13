618fcaaf3be3069b45565f1c46e3b2d1209847cf
Kristiyan Stoimenov <kristoimenov@gmail.com>
2023-11-13 14:08:06 +0200

    asm: Add bulk_sum() example
    This examples uses parameters which cannot all be contained in the volatile
    register set. Thus, some values are accessed using offset from the stack
    pointer.
    
    Signed-off-by: Kristiyan Stoimenov <kristoimenov@gmail.com>

--- examples/asm/main.cpp
+++ examples/asm/main.cpp
@@ -1,10 +1,15 @@
 #include <stdint.h>
 
+struct myint {
+	uint64_t v;
+};
+
 extern "C" {
 	int f(int rcx);
 	void swap(uint64_t *rdi, uint64_t *rsi);
 	uint64_t g(uint64_t arr[], uint64_t index);
 	uint64_t memcpy_dummy(char *dest, const char *src, uint64_t size);
+	uint64_t bulk_sum(int *rdi, char rsi, uint64_t rdx, short rcx, int r8, int r9, int sp8, struct myint *);
 }
 
 int main(int argc, char *argv[]) {
@@ -16,7 +21,11 @@ int main(int argc, char *argv[]) {
 	char arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 	char arr2[100];
 	// const uint64_t val = g(arr, 3);
-	memcpy_dummy(arr2, arr, 10);
+	// memcpy_dummy(arr2, arr, 10);
+
+	int i = 1;
+	struct myint v {.v = 0x10000000};
+	auto x = bulk_sum(&i, '\x10', 0x100000, 0x1000, 0x100, 0x1000000, 0x10000, &v);
 	return 0;
 }
 
--- examples/asm/plat.s
+++ examples/asm/plat.s
@@ -2,6 +2,7 @@
 	global swap
 	global g
 	global memcpy_dummy
+	global bulk_sum
 
 	segment .text
 f:
@@ -28,6 +29,18 @@ memcpy_dummy:
 	mov [rdi], al
 	add rdi, 1
 	add rsi, 1
-	sub rdx , 1
+	sub rdx, 1
 	jnz memcpy_dummy
+	ret
+
+bulk_sum:
+	movsx rax, dword [rdi]
+	add rax, rsi
+	add rax, rdx
+	add rax, rcx
+	add rax, r8
+	add rax, r9
+	add rax, [rsp+0x8]
+	mov rsi, [rsp+0x10]
+	add rax, [rsi]
 	ret
