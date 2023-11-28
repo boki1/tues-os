5c5db059cc91587e4252a768095a8228c2f0e21c
Kristiyan Stoimenov <kristoimenov@gmail.com>
2023-11-28 03:26:05 +0200

    mp-examples: Fix data race and add disclaimer
    Signed-off-by: Kristiyan Stoimenov <kristoimenov@gmail.com>

--- mp-examples/thread-count.cpp
+++ mp-examples/thread-count.cpp
@@ -1,11 +1,20 @@
 #include <thread>
 #include <iostream>
 #include <atomic>
+#include <mutex>
+
+/*
+ * This program has effectively no need for splitting into multiple threads
+ * due to the fact that the bottle neck isn't the CPU's capabilities.
+ */
 
 int main() {
-	std::atomic<uint64_t> l = 1ull << 10, r = 0;
+	std::atomic<int64_t> l = 1ull << 30, r = 0;
+	std::mutex m;
 	auto fun = [&]() {
-		while (l) --l, ++r;
+		std::unique_lock<std::mutex> _{m};
+		while (l > 0)
+			--l, ++r;
 	};
 	std::thread t1{fun};
 	std::thread t2{fun};
