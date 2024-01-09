db0f91d0061afdd3ef0c42670fcac47694ffcb67
Kristiyan Stoimenov <kristoimenov@gmail.com>
2024-01-09 23:50:16 +0200

    mp-examples: Make rwlock nicer
    Signed-off-by: Kristiyan Stoimenov <kristoimenov@gmail.com>

--- mp-examples/rwlock.cpp
+++ mp-examples/rwlock.cpp
@@ -2,76 +2,58 @@
 #include <cassert>
 #include <cstring>
 
-struct rwlock {
+//#define DEBUG
 
-    bool try_lock_shared()
-    {
+constexpr bool is_dbg() {
+#ifdef DEBUG
+    return true;
+#else
+    return false;
+#endif
+}
+
+#define assert_dbg(expr, msg) if constexpr (is_dbg()) if (!(expr)) { assert(!(msg)); }
+
+struct rw_lock {
+    bool lock_shared() {
         auto old_state = m_state.load(std::memory_order_acquire);
         if (old_state.exclusive)
             return false;
         state new_state = old_state;
         ++new_state.share_count;
-        return m_state.compare_exchange_strong(old_state, new_state,
-                                               std::memory_order_release, std::memory_order_acquire);
+        while (!m_state.compare_exchange_strong(old_state, new_state,
+                                                std::memory_order_acq_rel, std::memory_order_acquire));
+        return true;
     }
 
-    inline void lock_shared()
-    {
-        while (!try_lock_shared())
-            ;
-    }
-
-    void unlock_shared()
-    {
+    void unlock_shared() {
         auto old_state = m_state.load(std::memory_order_acquire);
-        if (old_state.exclusive || old_state.share_count == 0)
-            return;
+        assert_dbg(!old_state.exclusive && old_state.share_count > 0, "Bad unlock_shared() call.");
         state new_state = old_state;
         --new_state.share_count;
         while (!m_state.compare_exchange_strong(old_state, new_state,
-                                                std::memory_order_release, std::memory_order_acquire))
-            ;
+                                                std::memory_order_acq_rel, std::memory_order_acquire));
     }
 
-    bool try_lock()
-    {
-        auto old_state = m_state.load(std::memory_order_acquire);
-        if (old_state.exclusive || old_state.share_count > 0)
-            return false;
-        auto new_state = old_state;
-        new_state.exclusive = true;
-        return m_state.compare_exchange_strong(old_state, new_state,
-                                               std::memory_order_release, std::memory_order_acquire);
+    bool lock_exclusive() {
+        static const auto locked = [&] {
+            state s{};
+            std::memset(reinterpret_cast<uint8_t*>(&s), 0, sizeof(state));
+            s.exclusive = true;
+            return s;
+        }();
+
+        state expected_old = zero_state;
+        return m_state.compare_exchange_strong(expected_old, locked,
+                                               std::memory_order_acq_rel, std::memory_order_acquire);
     }
 
-    inline void lock()
-    {
-        while (!try_lock())
-            ;
-    }
-
-    void unlock()
-    {
-        auto old_state = m_state.load(std::memory_order_acquire);
-        if (!old_state.exclusive || old_state.share_count == 0)
-            return;
-        auto new_state = old_state;
-        new_state.exclusive = false;
-        while (!m_state.compare_exchange_strong(old_state, new_state,
-                                                std::memory_order_release, std::memory_order_acquire))
-            ;
-    }
-
-    bool try_upgrade()
-    {
-        auto old_state = m_state.load(std::memory_order_acquire);
-        if (old_state.exclusive || old_state.share_count > 1)
-            return false;
-        auto new_state = old_state;
-        new_state.exclusive = true;
-        new_state.share_count = 0;
-        return m_state.compare_exchange_strong(old_state, new_state,
-                                               std::memory_order_release, std::memory_order_acquire);
+    void unlock_exclusive() {
+#ifdef DEBUG
+            auto old_state = m_state.load(std::memory_order_acquire);
+            assert_dbg(old_state.exclusive && old_state.share_count == 0, "Bad unlock_exclusive() call");
+#endif
+        m_state.store(zero_state, std::memory_order_release);
     }
 
 private:
@@ -85,7 +67,7 @@ private:
     static_assert(std::atomic<state>::is_always_lock_free);
 
     static const inline auto zero_state = [] {
-        state s;
+        state s{};
         std::memset(reinterpret_cast<uint8_t*>(&s), 0, sizeof(state));
         return s;
     }();
@@ -93,12 +75,20 @@ private:
     std::atomic<state> m_state{zero_state};
 };
 
-int main()
-{
-	rwlock l;
-	l.lock_shared();
-	l.unlock_shared();
+int main() {
+    rw_lock rwl;
+    auto lx = rwl.lock_exclusive();
+    rwl.unlock_exclusive();
 
-	l.lock();
-	l.unlock();
+    auto ls = rwl.lock_shared();
+    rwl.unlock_shared();
+
+    rwl.lock_exclusive();
+    auto ls_fails = rwl.lock_shared();
+    assert(!ls_fails);
+    rwl.unlock_exclusive();
+
+    rwl.lock_shared();
+    auto lx_fails = rwl.lock_exclusive();
+    assert(!lx_fails);
 }
